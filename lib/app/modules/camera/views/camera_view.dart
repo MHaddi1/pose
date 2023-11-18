fb9d60839e1781e8222aab652d7a7be64cb31e59
MHaddi1 <se.m.haddi@gmail.com>
2023-11-18 20:32:31 +0500

    Pose

---
+++ lib/app/modules/camera/views/camera_view.dart
@@ -0,0 +1,203 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+import 'package:pose/app/data/components/coordinates_translator.dart';
+
+enum ScreenMode { gallery }
+
+class PosePainter extends CustomPainter {
+  PosePainter(this.poses, this.absoluteImageSize, this.rotation);
+
+  final List<Pose> poses;
+  final Size absoluteImageSize;
+  final InputImageRotation rotation;
+
+  double calculateDistance(PoseLandmark joint1, PoseLandmark joint2) {
+    return sqrt(pow(joint2.x - joint1.x, 2) + pow(joint2.y - joint1.y, 2));
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0
+      ..color = Colors.green;
+
+    final leftPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.yellow;
+
+    final rightPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.blueAccent;
+
+    for (final pose in poses) {
+      pose.landmarks.forEach((_, landmark) {
+        canvas.drawCircle(
+          Offset(
+            translateX(landmark.x, rotation, size, absoluteImageSize),
+            translateY(landmark.y, rotation, size, absoluteImageSize),
+          ),
+          1,
+          paint,
+        );
+      });
+
+      void paintLine(
+          PoseLandmarkType type1, PoseLandmarkType type2, Paint paintType) {
+        final PoseLandmark joint1 = pose.landmarks[type1]!;
+        final PoseLandmark joint2 = pose.landmarks[type2]!;
+        final double length = calculateDistance(joint1, joint2);
+
+        // Use the length variable as needed, e.g., print it or use it for further calculations
+        print('Length between $type1 and $type2: $length');
+
+        canvas.drawLine(
+          Offset(
+            translateX(joint1.x, rotation, size, absoluteImageSize),
+            translateY(joint1.y, rotation, size, absoluteImageSize),
+          ),
+          Offset(
+            translateX(joint2.x, rotation, size, absoluteImageSize),
+            translateY(joint2.y, rotation, size, absoluteImageSize),
+          ),
+          paintType,
+        );
+      }
+
+      // Draw arms
+      paintLine(
+          PoseLandmarkType.leftShoulder, PoseLandmarkType.leftElbow, leftPaint);
+      paintLine(
+          PoseLandmarkType.leftElbow, PoseLandmarkType.leftWrist, leftPaint);
+      paintLine(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightElbow,
+          rightPaint);
+      paintLine(
+          PoseLandmarkType.rightElbow, PoseLandmarkType.rightWrist, rightPaint);
+
+      // Draw Body
+      paintLine(
+          PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip, leftPaint);
+      paintLine(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightHip,
+          rightPaint);
+
+      // Draw legs
+      paintLine(
+          PoseLandmarkType.leftHip, PoseLandmarkType.leftAnkle, leftPaint);
+      paintLine(
+          PoseLandmarkType.rightHip, PoseLandmarkType.rightAnkle, rightPaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant PosePainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.poses != poses;
+  }
+}
+
+class CameraView extends StatefulWidget {
+  CameraView({
+    Key? key,
+    required this.title,
+    required this.customPaint,
+    this.text,
+    required this.onImage,
+    this.onScreenModeChanged,
+  }) : super(key: key);
+
+  final String title;
+  final CustomPaint? customPaint;
+  final String? text;
+  final Function(InputImage inputImage) onImage;
+  final Function(ScreenMode mode)? onScreenModeChanged;
+
+  @override
+  _CameraViewState createState() => _CameraViewState();
+}
+
+class _CameraViewState extends State<CameraView> {
+  File? _image;
+  String? _path;
+  ImagePicker? _imagePicker;
+
+  @override
+  void initState() {
+    super.initState();
+    _imagePicker = ImagePicker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _galleryBody(),
+    );
+  }
+
+  Widget _galleryBody() {
+    return ListView(shrinkWrap: true, children: [
+      _image != null
+          ? SizedBox(
+              height: 400,
+              width: 400,
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image.file(_image!),
+                  if (widget.customPaint != null) widget.customPaint!,
+                ],
+              ),
+            )
+          : Icon(
+              Icons.image,
+              size: 200,
+            ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: ElevatedButton(
+          child: Text('From Gallery'),
+          onPressed: () => _getImage(ImageSource.gallery),
+        ),
+      ),
+      if (_image != null)
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+              '${_path == null ? '' : 'Image path: $_path'}\n\n${widget.text ?? ''}'),
+        ),
+    ]);
+  }
+
+  Future _getImage(ImageSource source) async {
+    setState(() {
+      _image = null;
+      _path = null;
+    });
+    final pickedFile = await _imagePicker?.pickImage(source: source);
+    if (pickedFile != null) {
+      _processPickedFile(pickedFile);
+    }
+    setState(() {});
+  }
+
+  void _processPickedFile(XFile? pickedFile) async {
+    final path = pickedFile?.path;
+    if (path == null) {
+      return;
+    }
+    setState(() {
+      _image = File(path);
+    });
+    _path = path;
+    final inputImage = InputImage.fromFilePath(path);
+    widget.onImage(inputImage);
+  }
+}
